ca1de29284e4eea73e8c17c18071cd5d38c78a83
Jim McAndrew <jim@loc8.us>
2014-03-11 10:18:30 -0600

    Add in unit codes from either source

--- data-import/sql/table.sql
+++ data-import/sql/table.sql
@@ -1,36 +1,61 @@
---SELECT unit_code FROM irma_nps_boundaries;
---SELECT alpha FROM park_attributes;
---SELECT unit_code FROM wsd_polys;
-
-SELECT
-irma_wsd.geom as the_geom,
-park_attrs.*
-FROM
-(SELECT 
-  alpha, pointtopol, designation, name, display_name, display_designation, display_concatenated, display_state, display_blurb, display_url, display_address, display_phone, display_climate 
-  from park_attributes
-  group by alpha, pointtopol, designation, name, display_name, display_designation, display_concatenated, display_state, display_blurb, display_url, display_address, display_phone, display_climate
-  ) AS park_attrs FULL OUTER JOIN (
-  SELECT
-  --irma.unit_code,
-  --wsd.unit_code,
-  --irma.wkb_geometry as irma_geom,
-  --wsd.wkb_geometry as wsd_geom,
-  coalesce(wsd.unit_code, irma.unit_code) as unit_code,
-  coalesce(wsd.wkb_geometry, irma.wkb_geometry) as geom
-  FROM
-  (SELECT
-    irma_nps_boundaries.unit_code,
-    ST_Collect(array_agg(irma_nps_boundaries.wkb_geometry)) AS wkb_geometry
-    FROM
-    irma_nps_boundaries
-    GROUP BY
-    irma_nps_boundaries.unit_code) AS irma FULL OUTER JOIN 
-  (SELECT
-    wsd_polys.unit_code,
-    ST_Collect(array_agg(wsd_polys.wkb_geometry)) AS wkb_geometry
-    FROM
-    wsd_polys
-    GROUP BY
-    wsd_polys.unit_code) AS wsd
-  ON irma.unit_code = wsd.unit_code) AS irma_wsd ON park_attrs.alpha = irma_wsd.unit_code;
+SELECT irma_wsd.geom                                  AS the_geom,
+       Coalesce(irma_wsd.unit_code, park_attrs.alpha) AS unit_code,
+       park_attrs.pointtopol,
+       park_attrs.designation,
+       park_attrs.name,
+       park_attrs.display_name,
+       park_attrs.display_designation,
+       park_attrs.display_concatenated,
+       park_attrs.display_state,
+       park_attrs.display_blurb,
+       park_attrs.display_url,
+       park_attrs.display_address,
+       park_attrs.display_phone,
+       park_attrs.display_climate
+FROM   (SELECT alpha,
+               pointtopol,
+               designation,
+               name,
+               display_name,
+               display_designation,
+               display_concatenated,
+               display_state,
+               display_blurb,
+               display_url,
+               display_address,
+               display_phone,
+               display_climate
+        FROM   park_attributes
+        GROUP  BY alpha,
+                  pointtopol,
+                  designation,
+                  name,
+                  display_name,
+                  display_designation,
+                  display_concatenated,
+                  display_state,
+                  display_blurb,
+                  display_url,
+                  display_address,
+                  display_phone,
+                  display_climate) AS park_attrs
+       full outer join (SELECT
+                       --irma.unit_code,
+                       --wsd.unit_code,
+                       --irma.wkb_geometry as irma_geom,
+                       --wsd.wkb_geometry as wsd_geom,
+                       Coalesce(wsd.unit_code, irma.unit_code)       AS
+                       unit_code,
+                       Coalesce(wsd.wkb_geometry, irma.wkb_geometry) AS geom
+                        FROM   (SELECT irma_nps_boundaries.unit_code,
+       St_collect(Array_agg(irma_nps_boundaries.wkb_geometry)) AS
+       wkb_geometry
+       FROM   irma_nps_boundaries
+       GROUP  BY irma_nps_boundaries.unit_code) AS irma
+       full outer join (SELECT wsd_polys.unit_code,
+                       St_collect(Array_agg(wsd_polys.wkb_geometry)) AS
+       wkb_geometry
+                FROM   wsd_polys
+                GROUP  BY wsd_polys.unit_code) AS wsd
+            ON irma.unit_code = wsd.unit_code) AS irma_wsd
+                    ON park_attrs.alpha = irma_wsd.unit_code;
