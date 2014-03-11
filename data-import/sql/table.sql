6b20f3f900e4c676538918c663558e741357dc17
Jim McAndrew <jim@loc8.us>
2014-03-11 11:07:52 -0600

    Add source column

--- data-import/sql/table.sql
+++ data-import/sql/table.sql
@@ -1,5 +1,6 @@
 SELECT irma_wsd.poly_geom                                  AS poly_geom,
 irma_wsd.point_geom                                  AS point_geom,
+Coalesce(irma_wsd.poly_source, 'None') AS poly_source,
        Coalesce(irma_wsd.unit_code, park_attrs.alpha) AS unit_code,
        park_attrs.pointtopol,
        park_attrs.designation,
@@ -43,13 +44,18 @@ FROM   (SELECT alpha,
        full outer join (
        SELECT
          Coalesce(poly.unit_code, point.unit_code) as unit_code,
+         poly.source as poly_source,
          poly.geom as poly_geom,
          point.wkb_geometry as point_geom
        FROM
          wsd_points AS point FULL OUTER JOIN
        (SELECT
-                       Coalesce(wsd.unit_code, irma.unit_code)       AS
-                       unit_code,
+                       Coalesce(wsd.unit_code, irma.unit_code)       AS unit_code,
+                        CASE 
+                          WHEN (wsd.wkb_geometry IS NOT NULL) then 'WSD_Parks'
+                          WHEN (irma.wkb_geometry IS NOT NULL) then 'IRMA'
+                          ELSE 'Unknown'
+                        END as source,
                        Coalesce(wsd.wkb_geometry, irma.wkb_geometry) AS geom
                         FROM   (SELECT irma_nps_boundaries.unit_code,
        St_collect(Array_agg(irma_nps_boundaries.wkb_geometry)) AS
@@ -65,4 +71,5 @@ FROM   (SELECT alpha,
                     ON park_attrs.alpha = irma_wsd.unit_code
        WHERE
          display_designation != 'National Historic Trail' AND
-         display_designation != 'National Scenic Trail';
+         display_designation != 'National Scenic Trail' AND
+         unit_code != 'SEKI';
