e7d980d4c3471b439ba826d74374b9f4eef22819
Jim McAndrew <jim@loc8.us>
2014-03-28 13:28:57 -0600

    Update tm2_queries.sql

--- data-import/data/tm2_queries.sql
+++ data-import/data/tm2_queries.sql
@@ -10,3 +10,13 @@ as data
 
 (select way, "fcategory", name, z_order from planet_osm_point order by z_order desc) as data
 
+-- nps_test_points
+
+(select point_geom, 
+minzoompoly, 
+buffer_rank_750km,
+name, 
+display_name, 
+display_concatenated, 
+display_designation 
+from npmap_all_parks) as data
