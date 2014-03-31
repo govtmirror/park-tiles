0a4eb06d119f44a86d494a517b0efe45c2252881
Jim McAndrew <jim@loc8.us>
2014-03-31 11:34:39 -0600

    Add nps_test_park_label query

--- data-import/data/tm2_queries.sql
+++ data-import/data/tm2_queries.sql
@@ -20,3 +20,18 @@ display_name,
 display_concatenated, 
 display_designation 
 from npmap_all_parks) as data
+
+-- nps_test_park_label
+
+(select 
+label_point,
+name, 
+display_name, 
+display_concatenated,
+visitor_area_rank_100km,
+visitor_area_rank_250km,
+visitor_area_rank_500km,
+visitor_area_rank_750km,
+region_rank,
+minzoompoly from npmap_all_parks)
+as data
