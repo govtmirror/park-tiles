3dc99eae9440f8541280d9a3490fad58d4f86962
Jim McAndrew <jim@loc8.us>
2014-04-03 15:11:07 -0600

    Update larger_distance.sql

--- data-import/sql/import/scripts/functions/larger_distance.sql
+++ data-import/sql/import/scripts/functions/larger_distance.sql
@@ -21,3 +21,40 @@ SET
   WHERE
     a.area > label_points.area
 );
+
+
+ALTER TABLE label_points ADD COLUMN area_direction numeric;
+UPDATE
+  label_points d
+SET
+  area_direction = (
+    SELECT dir from (
+      SELECT
+        b.unit_code,
+        row_number() OVER (order by st_distance(a.point_geom, b.point_geom)) rank,
+        st_distance(a.point_geom, b.point_geom) dist,
+        degrees(st_azimuth(st_closestpoint(a.point_geom, b.point_geom), st_closestpoint(b.point_geom, a.point_geom))) dir
+      FROM
+        label_points a JOIN label_points b ON a.unit_code != b.unit_code
+      WHERE
+        a.unit_code = d.unit_code AND
+        coalesce(d.area, 1) > coalesce(a.area, 0)
+     ) c WHERE rank = 1);
+     
+ALTER TABLE label_points ADD COLUMN visitors_direction numeric;
+UPDATE
+  label_points d
+SET
+  visitors_direction = (
+    SELECT dir from (
+      SELECT
+        b.unit_code,
+        row_number() OVER (order by st_distance(a.point_geom, b.point_geom)) rank,
+        st_distance(a.point_geom, b.point_geom) dist,
+        degrees(st_azimuth(st_closestpoint(a.point_geom, b.point_geom), st_closestpoint(b.point_geom, a.point_geom))) dir
+      FROM
+        label_points a JOIN label_points b ON a.unit_code != b.unit_code
+      WHERE
+        a.unit_code = d.unit_code AND
+        coalesce(d.visitors, 1) > coalesce(a.visitors, 0)
+     ) c WHERE rank = 1);
