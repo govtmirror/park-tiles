bcfef30c6059682d743d7a0e1d7cc847a1bae66d
Jim McAndrew <jim@loc8.us>
2014-04-03 14:08:58 -0600

    Create larger_distance.sql

---
+++ data-import/sql/import/scripts/functions/larger_distance.sql
@@ -0,0 +1,23 @@
+ALTER TABLE label_points ADD COLUMN visitors_dist numeric;
+UPDATE
+  label_points
+SET
+  visitors_dist = (SELECT 
+    COALESCE(MIN(ST_Distance(a.point_geom, label_points.point_geom)),10000) as visitors_dist
+  FROM
+    label_points a 
+  WHERE
+    a.visitors > coalesce(label_points.visitors, 0)
+);
+
+ALTER TABLE label_points ADD COLUMN area_dist numeric;
+UPDATE
+  label_points
+SET
+  area_dist = (SELECT 
+    COALESCE(MIN(ST_Distance(a.point_geom, label_points.point_geom)),10000) as area_dist
+  FROM
+    label_points a 
+  WHERE
+    a.area > label_points.area
+);
